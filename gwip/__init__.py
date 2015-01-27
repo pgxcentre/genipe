d4e50646b6bc6a1f5cdd0b158656beb32eeafbfc
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2015-01-27 11:43:11 -0500

    New way to get version

--- gwip/__init__.py
+++ gwip/__init__.py
@@ -4,8 +4,14 @@
 __author__ = "Louis-Philippe Lemieux Perreault"
 __copyright__ = "Copyright 2014, Beaulieu-Saucier Pharmacogenomics Centre"
 __credits__ = ["Louis-Philippe Lemieux Perreault", "Ian Mongrain"]
-__license__ = "GPL"
-__version__ = "0.1"
+__license__ = "MIT"
 __maintainer__ = "Louis-Philippe Lemieux Perreault"
 __email__ = "louis-philippe.lemieux.perreault@statgen.org"
 __status__ = "Development"
+
+
+# Loading the version
+try:
+    from .version import gwip_version as __version__
+except ImportError:
+    __version__ = None
--- setup.py
+++ setup.py
@@ -5,27 +5,59 @@
 # python setup.py sdist --format gztar
 # python setup.py sdist --format zip
 
+
 import os
 from setuptools import setup
 
 from gwip import __version__
 
-setup(
-    name="gwip",
-    version=__version__,
-    description="Genome-Wide Imputation Pipeline.",
-    author="Louis-Philippe Lemieux Perreault",
-    author_email="louis-philippe.lemieux.perreault@statgen.org",
-    url="http://www.statgen.org",
-    license="GPL",
-    entry_points={
-        "console_scripts": ["gwip-launcher=gwip.pipeline:main",
-                            "impute2-merger=gwip.tool.impute2_merger:main", ],
-    },
-    install_requires=["numpy >= 1.8.2", "jinja2 >= 2.7.3", ],
-    packages=["gwip", "gwip.task", "gwip.db", "gwip.tool", "gwip.reporting", ],
-    package_data={"gwip.reporting": ["template/*.tex", ], },
-    classifiers=["Operating System :: Linux",
-                 "Programming Language :: Python",
-                 "Programming Language :: Python :: 3", ],
-)
+
+MAJOR = 0
+MINOR = 1
+VERSION = "{}.{}".format(MAJOR, MINOR)
+
+
+def write_version_file(fn=os.path.join("gwip", "version.py")):
+    content = """
+# THIS FILE WAS GENERATED AUTOMATICALLY BY GWIP SETUP.PY
+gwip_version = {version}
+"""
+    a = Noneopen(fn, "w")
+    try:
+        a.write(content.format(version=VERSION))
+    finally:
+        a.close()
+
+
+def setup_package():
+    # Saving the version into a file
+    write_version_file()
+
+    setup(
+        name="gwip",
+        version=VERSION,
+        description="PGx Genome-Wide Imputation Pipeline.",
+        author="Louis-Philippe Lemieux Perreault",
+        author_email="louis-philippe.lemieux.perreault@statgen.org",
+        url="http://www.statgen.org",
+        license="MIT",
+        entry_points={
+            "console_scripts": [
+                "gwip-launcher=gwip.pipeline:main",
+                "impute2-merger=gwip.tool.impute2_merger:main",
+            ],
+        },
+        install_requires=["numpy >= 1.8.2", "jinja2 >= 2.7.3", ],
+        packages=["gwip", "gwip.task", "gwip.db", "gwip.tool",
+                  "gwip.reporting", ],
+        package_data={"gwip.reporting": ["template/*.tex", ], },
+        classifiers=["Operating System :: Linux",
+                     "Programming Language :: Python",
+                     "Programming Language :: Python :: 3", ],
+    )
+
+    return
+
+
+if __name__ == "__main__":
+    setup_package()
