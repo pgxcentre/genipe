5f22f50006918f8c1f3507a8b6b6ac43b67faae8
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2015-02-09 13:53:36 -0500

    Added license and author info at every py file

--- gwip/reporting/utils.py
+++ gwip/reporting/utils.py
@@ -1,4 +1,16 @@
 
+# This file is part of gwip.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to Creative
+# Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+
+__author__ = "Louis-Philippe Lemieux Perreault"
+__copyright__ = "Copyright 2014, Beaulieu-Saucier Pharmacogenomics Centre"
+__license__ = "Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)"
+
 
 __all__ = ["config_jinja2", "sanitize_tex", "format_tex", "wrap_tex",
            "create_tabular", "create_float", "tex_inline_math", ]
