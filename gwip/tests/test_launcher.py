4f53a51d41e8bfde8ed64ad972e8a25fda3d931d
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2015-02-23 11:39:45 -0500

    Added testing for the launcher module

--- gwip/tests/test_launcher.py
+++ gwip/tests/test_launcher.py
@@ -7,8 +7,12 @@
 # Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
 
+import os
+import shutil
 import unittest
 from tempfile import TemporaryDirectory
+
+from ..task.launcher import _check_output_files
 
 
 __author__ = "Louis-Philippe Lemieux Perreault"
@@ -28,7 +32,28 @@ class TestLauncher(unittest.TestCase):
         # Deleting the output directory
         self.output_dir.cleanup()
 
-    @unittest.skip("Test not implemented")
     def test_check_output_files(self):
         """Tests the '_check_output_files' function."""
-        self.fail("Test not implemented")
+        # First a list of files were they are all available
+        filenames = [
+            os.path.join(self.output_dir.name, "file_{}.txt".format(i))
+            for i in range(1, 11)
+        ]
+        for filename in filenames:
+            with open(filename, "w") as o_file:
+                pass
+        self.assertTrue(_check_output_files(filenames))
+
+        # Adding an 'impute2' file
+        filenames.append(os.path.join(self.output_dir.name, "test.impute2"))
+        with open(filenames[-1], "w") as o_file:
+            pass
+        self.assertTrue(_check_output_files(filenames))
+
+        # 'Compressing' the file
+        shutil.move(filenames[-1], filenames[-1] + ".gz")
+        self.assertTrue(_check_output_files(filenames))
+
+        # Deleting a file
+        os.remove(filenames[0])
+        self.assertFalse(_check_output_files(filenames))
