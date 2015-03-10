d9a827397059a0bf4c282305ee5e50b295dad4fe
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2015-03-10 08:19:06 -0400

    Bug when MAF is NA for whatever reason

--- gwip/tools/imputed_stats.py
+++ gwip/tools/imputed_stats.py
@@ -343,7 +343,7 @@ def process_impute2_site(site_info):
                  allele_encoding[minor], maf, data.shape[0]]
 
     # If the marker is to rare, we continue with the rest
-    if maf < site_info.maf_t:
+    if (maf == "NA") or (maf < site_info.maf_t):
         to_return.extend(["NA"] * (site_info.number_to_print - len(to_return)))
         return to_return
 
